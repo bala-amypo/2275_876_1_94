25dcd577b10892a89745175e37f2f116f0dd432a
bala-amypo <bala@amypo.in>
2025-12-18 17:27:14 +0000

    User commit

--- src/main/java/com/example/demo/model/Zone.java
+++ src/main/java/com/example/demo/model/Zone.java
@@ -50,5 +50,11 @@ public class Zone {
     public void setActive(Boolean active) { 
         this.active = active; 
     }
+    public Zone getZone() {
+        return zone;
+    }
+    public void setZone(Zone zone) {
+        this.zone = zone;
+    }
 
 }
