633d784187236fc517deb6f2a36ca50aefc1800d
bala-amypo <bala@amypo.in>
2025-12-18 10:19:52 +0000

    User commit

--- src/main/java/com/example/demo/model/Zone.java
+++ src/main/java/com/example/demo/model/Zone.java
@@ -1,8 +1,13 @@
 package com.yourpackage.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Zone {
 
     @Id
@@ -17,43 +22,43 @@ public class Zone {
     @Column(nullable = false)
     private Boolean active = true;
 
-    // Default Constructor
-    public Zone() {}
+    // // Default Constructor
+    // public Zone() {}
 
-    // Parameterized Constructor
-    public Zone(String zoneName, String description) {
-        this.zoneName = zoneName;
-        this.description = description;
-        this.active = true;
-    }
+    // // Parameterized Constructor
+    // public Zone(String zoneName, String description) {
+    //     this.zoneName = zoneName;
+    //     this.description = description;
+    //     this.active = true;
+    // }
 
-    // Getters & Setters
-    public Long getId() { 
-        return id; 
-    }
-    public void setId(Long id) { 
-        this.id = id; 
-    }
+    // // Getters & Setters
+    // public Long getId() { 
+    //     return id; 
+    // }
+    // public void setId(Long id) { 
+    //     this.id = id; 
+    // }
 
-    public String getZoneName() { 
-        return zoneName; 
-    }
-    public void setZoneName(String zoneName) { 
-        this.zoneName = zoneName; 
-    }
+    // public String getZoneName() { 
+    //     return zoneName; 
+    // }
+    // public void setZoneName(String zoneName) { 
+    //     this.zoneName = zoneName; 
+    // }
 
-    public String getDescription() { 
-        return description; 
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    // public String getDescription() { 
+    //     return description; 
+    // }
+    // public void setDescription(String description) {
+    //     this.description = description;
+    // }
 
-    public Boolean getActive() { 
-        return active; 
-    }
-    public void setActive(Boolean active) { 
-        this.active = active; 
-    }
+    // public Boolean getActive() { 
+    //     return active; 
+    // }
+    // public void setActive(Boolean active) { 
+    //     this.active = active; 
+    // }
 
 }
