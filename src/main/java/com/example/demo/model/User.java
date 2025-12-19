ebbc3ddb9c18a3606385a1d0f020fbc9c393722c
bala-amypo <bala@amypo.in>
2025-12-19 08:39:32 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,8 +1,9 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -11,60 +12,55 @@ public class User {
 
     private String fullName;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false)
     private String role;
 
-    // Default Constructor
-    public User() {}
+    public User() {
+    }
 
-    // Parameterized Constructor
-    public User(String fullName, String email,String password, String role) {
+    public User(String fullName, String email, String password, String role) {
         this.fullName = fullName;
         this.email = email;
         this.password = password;
         this.role = role;
     }
 
-    // Getters & Setters
-    public Long getId() { 
-        return id; 
-    }
-    public void setId(Long id) { 
-        this.id = id; 
+    public Long getId() {
+        return id;
     }
 
-    public String getFullName() { 
-        return fullName; 
+    public String getFullName() {
+        return fullName;
     }
+
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
-    public String getEmail() { 
-        return email; 
+    public String getEmail() {
+        return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() { 
-        return password; 
+    public String getPassword() {
+        return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getRole() { 
-        return role; 
+    public String getRole() {
+        return role;
     }
+
     public void setRole(String role) {
         this.role = role;
     }
-
 }
