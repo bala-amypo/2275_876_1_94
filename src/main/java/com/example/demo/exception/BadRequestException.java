5105f822cecff838fc5a33d16fd2db60f63de403
bala-amypo <bala@amypo.in>
2025-12-19 17:30:21 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,12 +1,12 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-// @ResponseStatus(HttpStatus.BAD_REQUEST)
-// public class BadRequestException extends RuntimeException {
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
 
-//     public BadRequestException(String message) {
-//         super(message);
-//     }
-// }
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
