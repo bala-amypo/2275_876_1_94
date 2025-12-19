db123026014dc24b8d33eb53f089737d4bf1fdbc
bala-amypo <bala@amypo.in>
2025-12-19 17:32:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,22 @@
+// package com.example.demo.controller;
+
+// import com.example.demo.security.JwtTokenProvider;
+// import com.example.demo.service.UserService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
+
+//     @Autowired
+//     private UserService userService;
+
+//     @Autowired
+//     private JwtTokenProvider jwtTokenProvider;
+
+//     @GetMapping("/test")
+//     public String test() {
+//         return "Auth OK";
+//     }
+// }
