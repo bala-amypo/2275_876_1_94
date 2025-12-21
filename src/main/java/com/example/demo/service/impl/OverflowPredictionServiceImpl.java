2fd7fe21e455c92bd75083be540ae2dd73ac54de
bala-amypo <bala@amypo.in>
2025-12-21 09:05:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/OverflowPredictionServiceImpl.java
+++
@@ -1,8 +0,0 @@
-@Override
-public List<OverflowPrediction> getLatestPredictionsForZone(Long zoneId) {
-
-    Zone zone = zoneRepository.findById(zoneId)
-            .orElseThrow(() -> new ResourceNotFoundException("Zone not found"));
-
-    return predictionRepository.findLatestPredictionsForZone(zone);
-}
