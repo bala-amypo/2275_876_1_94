0c1aa18937e9dc090e9486e43b092d4c7f6420d7
bala-amypo <bala@amypo.in>
2025-12-21 16:19:20 +0000

    User commit

--- src/main/java/com/example/demo/model/Zone.java
+++ src/main/java/com/example/demo/model/Zone.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Zone {
 
     @Id
@@ -14,35 +20,4 @@ public class Zone {
 
     private String name;
     private String location;
-
-    // Default constructor
-    public Zone() {}
-
-    // Constructor with fields
-    public Zone(String name, String location) {
-        this.name = name;
-        this.location = location;
-    }
-
-    // Getters
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
