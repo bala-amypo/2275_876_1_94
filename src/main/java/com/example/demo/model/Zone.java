86d315441a2cc4bed2bf2a894747f899bd912136
bala-amypo <bala@amypo.in>
2025-12-21 13:31:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Zone.java
@@ -0,0 +1,55 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "zones")
+public class Zone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String zoneName;
+
+    private String description;
+
+    private Boolean active = true;
+
+    public Zone() {}
+
+    public Zone(String zoneName, String description, Boolean active) {
+        this.zoneName = zoneName;
+        this.description = description;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+ 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+ 
+    public Boolean getActive() {
+        return active;
+    }
+ 
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
