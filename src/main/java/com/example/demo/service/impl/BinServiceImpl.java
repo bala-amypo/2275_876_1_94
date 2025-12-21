4d92d14821f461e7de4bf90791f7aca5d6fa2d83
bala-amypo <bala@amypo.in>
2025-12-21 13:43:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BinServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BinServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
 import com.example.demo.service.BinService;
--- src/main/java/com/example/demo/service/impl/UsagePatternModelServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsagePatternModelServiceImpl.java
@@ -1,4 +1,6 @@
 package com.example.demo.service.impl;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
