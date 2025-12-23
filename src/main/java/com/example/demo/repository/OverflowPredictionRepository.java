eac0d2c65e3ee7ec10b15adab22217fdf08a8998
bala-amypo <bala@amypo.in>
2025-12-23 09:07:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
+++ src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
+import com.example.demo.model.Bin;
+import com.example.demo.model.OverflowPrediction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-public interface OverflowPredictionRepository extends JpaRepository<OverflowPrediction, Long> {
-    List<OverflowPrediction> findLatestPredictionsForZone(Zone zone);
+@Repository
+public interface OverflowPredictionRepository
+        extends JpaRepository<OverflowPrediction, Long> {
+
+    List<OverflowPrediction> findByBin(Bin bin);
 }
