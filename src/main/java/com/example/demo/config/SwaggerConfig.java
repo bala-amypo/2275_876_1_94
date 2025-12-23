1db66106df30ffc58f65d9682485626d718aef7d
bala-amypo <bala@amypo.in>
2025-12-23 18:20:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bin Overflow Predictor API")
+                        .description("Predicts when city trash bins will overflow")
+                        .version("1.0"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth"))
+                .schemaRequirement("bearerAuth", bearerScheme);
+    }
+}
