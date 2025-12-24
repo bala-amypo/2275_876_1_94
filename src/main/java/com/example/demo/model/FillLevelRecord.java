6cff4a7f84afc84d813ea904f879a1b14b4a5b40
bala-amypo <bala@amypo.in>
2025-12-24 06:03:32 +0000

    User commit

--- src/main/java/com/example/demo/model/FillLevelRecord.java
+++ src/main/java/com/example/demo/model/FillLevelRecord.java
@@ -6,45 +6,39 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "fill_level_records")
 public class FillLevelRecord {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @ManyToOne
+    @JoinColumn(name = "bin_id")
     private Bin bin;
-
+    
     private Double fillPercentage;
     private Timestamp recordedAt;
     private Boolean isWeekend;
-
+    
     public FillLevelRecord() {}
-
-    public FillLevelRecord(Bin bin, Double fillPercentage,
-                           Timestamp recordedAt, Boolean isWeekend) {
+    
+    public FillLevelRecord(Bin bin, Double fillPercentage, Timestamp recordedAt, Boolean isWeekend) {
         this.bin = bin;
         this.fillPercentage = fillPercentage;
         this.recordedAt = recordedAt;
         this.isWeekend = isWeekend;
     }
-
+    
     public Long getId() { return id; }
-
+    public void setId(Long id) { this.id = id; }
+    
     public Bin getBin() { return bin; }
     public void setBin(Bin bin) { this.bin = bin; }
-
+    
     public Double getFillPercentage() { return fillPercentage; }
-    public void setFillPercentage(Double fillPercentage) {
-        this.fillPercentage = fillPercentage;
-    }
-
+    public void setFillPercentage(Double fillPercentage) { this.fillPercentage = fillPercentage; }
+    
     public Timestamp getRecordedAt() { return recordedAt; }
-    public void setRecordedAt(Timestamp recordedAt) {
-        this.recordedAt = recordedAt;
-    }
-
+    public void setRecordedAt(Timestamp recordedAt) { this.recordedAt = recordedAt; }
+    
     public Boolean getIsWeekend() { return isWeekend; }
-    public void setIsWeekend(Boolean isWeekend) {
-        this.isWeekend = isWeekend;
-    }
+    public void setIsWeekend(Boolean isWeekend) { this.isWeekend = isWeekend; }
 }
