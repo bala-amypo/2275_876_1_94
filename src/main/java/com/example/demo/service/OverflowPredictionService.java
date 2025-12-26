0247176467bb7bba8453d6c45edcaff4aab8d476
bala-amypo <bala@amypo.in>
2025-12-26 16:49:38 +0000

    User commit

--- src/main/java/com/example/demo/service/OverflowPredictionService.java
+++ src/main/java/com/example/demo/service/OverflowPredictionService.java
@@ -1,11 +1,17 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import com.example.demo.model.OverflowPrediction;
-import java.util.List;
+// import com.example.demo.model.OverflowPrediction;
+// import java.util.List;
 
-public interface OverflowPredictionService {
-    OverflowPrediction generatePrediction(Long binId);
-    OverflowPrediction getPredictionById(Long id);
-    List<OverflowPrediction> getPredictionsForBin(Long binId);
-    List<OverflowPrediction> getLatestPredictionsForZone(Long zoneId);
-}
+// public interface OverflowPredictionService {
+//     OverflowPrediction generatePrediction(Long binId);
+//     OverflowPrediction getPredictionById(Long id);
+//     List<OverflowPrediction> getPredictionsForBin(Long binId);
+//     List<OverflowPrediction> getLatestPredictionsForZone(Long zoneId);
+// }
+
+
+
+
+
+
