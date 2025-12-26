8d7771e8f12aa376788986941528aac3d9e57532
bala-amypo <bala@amypo.in>
2025-12-26 17:04:38 +0000

    User commit

--- src/main/java/com/example/demo/service/ZoneService.java
+++ src/main/java/com/example/demo/service/ZoneService.java
@@ -16,3 +16,15 @@
 
 
 
+package com.example.demo.service;
+
+import com.example.demo.model.Zone;
+import java.util.List;
+
+public interface ZoneService {
+    Zone createZone(Zone zone);
+    Zone updateZone(Long id, Zone zone);
+    Zone getZoneById(Long id);
+    List<Zone> getAllZones();
+    void deactivateZone(Long id);
+}
