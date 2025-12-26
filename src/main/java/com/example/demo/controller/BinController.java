dcec62a41bc0c44c2b9d96403abdc2da880c98be
bala-amypo <bala@amypo.in>
2025-12-26 17:16:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/BinController.java
+++ src/main/java/com/example/demo/controller/BinController.java
@@ -1,52 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.model.Bin;
-// import com.example.demo.service.BinService;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/bins")
-// public class BinController {
-//     private final BinService binService;
-    
-//     public BinController(BinService binService) {
-//         this.binService = binService;
-//     }
-    
-//     @PostMapping
-//     public ResponseEntity<Bin> createBin(@RequestBody Bin bin) {
-//         return ResponseEntity.ok(binService.createBin(bin));
-//     }
-    
-//     @PutMapping("/{id}")
-//     public ResponseEntity<Bin> updateBin(@PathVariable Long id, @RequestBody Bin bin) {
-//         return ResponseEntity.ok(binService.updateBin(id, bin));
-//     }
-    
-//     @GetMapping("/{id}")
-//     public ResponseEntity<Bin> getBin(@PathVariable Long id) {
-//         return ResponseEntity.ok(binService.getBinById(id));
-//     }
-    
-//     @GetMapping
-//     public ResponseEntity<List<Bin>> getAllBins() {
-//         return ResponseEntity.ok(binService.getAllBins());
-//     }
-    
-//     @PutMapping("/{id}/deactivate")
-//     public ResponseEntity<Void> deactivateBin(@PathVariable Long id) {
-//         binService.deactivateBin(id);
-//         return ResponseEntity.ok().build();
-//     }
-// }
-
-
-
-
-
-
 package com.example.demo.controller;
 
 import com.example.demo.model.Bin;
@@ -90,9 +41,3 @@ public class BinController {
         return ResponseEntity.ok().build();
     }
 }
-
-
-
-
-
-
--- src/main/java/com/example/demo/controller/FillLevelRecordController.java
+++ src/main/java/com/example/demo/controller/FillLevelRecordController.java
@@ -1,53 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.model.FillLevelRecord;
-// import com.example.demo.service.FillLevelRecordService;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/fill-records")
-// public class FillLevelRecordController {
-//     private final FillLevelRecordService recordService;
-    
-//     public FillLevelRecordController(FillLevelRecordService recordService) {
-//         this.recordService = recordService;
-//     }
-    
-//     @PostMapping
-//     public ResponseEntity<FillLevelRecord> createRecord(@RequestBody FillLevelRecord record) {
-//         return ResponseEntity.ok(recordService.createRecord(record));
-//     }
-    
-//     @GetMapping("/{id}")
-//     public ResponseEntity<FillLevelRecord> getRecord(@PathVariable Long id) {
-//         return ResponseEntity.ok(recordService.getRecordById(id));
-//     }
-    
-//     @GetMapping("/bin/{binId}")
-//     public ResponseEntity<List<FillLevelRecord>> getRecordsForBin(@PathVariable Long binId) {
-//         return ResponseEntity.ok(recordService.getRecordsForBin(binId));
-//     }
-    
-//     @GetMapping("/bin/{binId}/recent")
-//     public ResponseEntity<List<FillLevelRecord>> getRecentRecords(@PathVariable Long binId, 
-//                                                                  @RequestParam(defaultValue = "10") int limit) {
-//         return ResponseEntity.ok(recordService.getRecentRecords(binId, limit));
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
 package com.example.demo.controller;
 
 import com.example.demo.model.FillLevelRecord;
@@ -85,6 +35,3 @@ public class FillLevelRecordController {
         return ResponseEntity.ok(recordService.getRecentRecords(binId, limit));
     }
 }
-
-
-
